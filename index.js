1234d4c876826f3af6ed71fde2aa7a195920a6ff
nshel <105758041+nshel@users.noreply.github.com>
2022-05-20 23:45:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -78,3 +78,32 @@ function myFilter() {
         }
     }
 }
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stick()};
+
+// Get the navbar
+var navbar = document.getElementById("topNav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stick() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// function for opening the left nav
+function openLeftNav() {
+
+    document.getElementById('leftNav').style.display = 'block';
+}
+// for closeing the left nav
+function closeLeftNav() {
+    document.getElementById('leftNav').style.display = 'none';
+}
