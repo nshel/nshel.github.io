8600ad8ab73421904a49438b08870ea11bf18688
nshel <105758041+nshel@users.noreply.github.com>
2022-05-21 03:10:10 +0100

    Add files via upload

--- programming/java/java.js
+++ programming/java/java.js
@@ -1,7 +1,7 @@
 
 //patterns
-let keywordsPattern1 = /\b(import|public|class|static|void|int|float|boolean)(?![\w])/g,
-keywordsPattern2 = /\b(try|if|else|switch|while|for|new|return|true|false)(?![\w])/g,
+var keywordsPattern1 = /\b(import|public|class|static|void|int|float|boolean)(?![\w])/g,
+keywordsPattern2 = /\b(try|if|else|switch|while|for|do|new|return|true|false)(?![\w])/g,
 strPattern1 = /(".*?")/g,
 strPattern2 = /('.*?')/g,
 sLineCommentPattern = /(\/\/.*)/g,
@@ -11,9 +11,10 @@ numbersPattern = /([0-9])/g;
 function javaSyntax() {
 										
 	// get all elements with id 'java-code'
-	let codeElements = document.querySelectorAll("[id=java-code]");
-	codeElements.forEach( element => {
-		let parsed = element.innerHTML;
+	var codeElements = document.querySelectorAll("[id=java-code]");
+	for (var i = 0; i < codeElements.length; i++) {
+
+		var parsed = codeElements[i].innerHTML;
 
 		parsed = parsed.replace(strPattern1, '<span id="string">$1</span>');
 		parsed = parsed.replace(strPattern2, "<span id='string'>$1</span>");
@@ -27,28 +28,31 @@ function javaSyntax() {
 		 * We need to recheck and replace some keywords, numbers in
 		 * comment so that it'll not be highlighted
 		 */
-		let res1 = parsed.match(sLineCommentPattern);
-		let res2 = parsed.match(mLineCommentPattern);
+		var res1 = parsed.match(sLineCommentPattern);
+		var res2 = parsed.match(mLineCommentPattern);
 		parsed = RegExD(res1, parsed);
 		parsed = RegExD(res2, parsed);
 
-		// let res3 = parsed.match(strPattern1);
+		// var res3 = parsed.match(strPattern1);
 		// parsed = RegExS(res3, parsed);
 
-		// let res4 = parsed.match(strPattern2);
+		// var res4 = parsed.match(strPattern2);
 		// parsed = RegExS(res4, parsed);
 
-		element.innerHTML = parsed;
-	});
+		codeElements[i].innerHTML = parsed;
+	}
+	// codeElements.forEach( element => {
+		
+	// });
 				
 }
 
 function RegExD(res, parsed) {
 
-	let p = parsed;
+	var p = parsed;
 	if (res != null) {
 		res.forEach(item => {
-			let n = item.replace(keywordsPattern1, "<span id='comment'>$1</span>");
+			var n = item.replace(keywordsPattern1, "<span id='comment'>$1</span>");
 			n = n.replace(keywordsPattern2, "<span id='comment'>$1</span>");
 			n = n.replace(numbersPattern, "<span id='comment'>$1</span>");
 			p = p.replace(item, n);
@@ -59,10 +63,10 @@ function RegExD(res, parsed) {
 
 function RegExS(res, parsed) {
 
-	let p = parsed;
+	var p = parsed;
 	if (res != null) {
 		res.forEach(item => {
-			let n = item.replace(keywordsPattern1, '<span id="string">$1</span>');
+			var n = item.replace(keywordsPattern1, '<span id="string">$1</span>');
 			n = n.replace(keywordsPattern2, '<span id="string">$1</span>');
 			n = n.replace(numbersPattern, '<span id="string">$1</span>');
 			p = p.replace(item, n);
