950e5c781cbbcf05a1dcb1eff1733d039a26a335
nshel <105758041+nshel@users.noreply.github.com>
2022-05-21 03:11:29 +0100

    Add files via upload

--- programming/python/py.js
+++ programming/python/py.js
@@ -1,19 +1,18 @@
 // Patterns
-let = keywordsPattern = /\b(import|from|if|else|elif|try|except|for|in|as|class|with|def|return|break|continue)(?![\w])/g,
+var keywordsPattern = /\b(import|from|if|else|elif|try|except|for|in|as|class|with|def|return|break|continue)(?![\w])/g,
 funcPattern = /\b(print|open|read|range|write|len|exit|input|str|int|self)(?![\w])/g,
 strPattern1 = /(".*?")/g,
 strPattern2 = /('.*?')/g,
-commentPattern = /(\#.*)/g,
-formatedStrPattern = //g,
+commentPattern = /(\#.*)/g
+// formatedStrPattern = //g,
 numbersPattern = /([0-9])/g;
 
 function pySyntax() {
 
     // get all elements with id 'py-code'
-    let codeElements = document.querySelectorAll("[id=py-code]");
-    codeElements.forEach(element => {
-        
-        let parsed = element.innerHTML;
+    var codeElements = document.querySelectorAll("[id=py-code]");
+	for (var i = 0; i < codeElements.length; i++) {
+		var parsed = codeElements[i].innerHTML;
         parsed = parsed.replace(strPattern1, '<span id="string">$1</span>');
         parsed = parsed.replace(strPattern2, "<span id=\"string\">$1</span>");
         parsed = parsed.replace(keywordsPattern, '<span id="keyword">$1</span>');
@@ -25,26 +24,30 @@ function pySyntax() {
 		 * We need to recheck and replace some keywords, numbers in
 		 * comment so that it'll not be highlighted
 		 */
-		let res1 = parsed.match(commentPattern);
+		var res1 = parsed.match(commentPattern);
 		parsed = RegExD(res1, parsed);
 
-		let res2 = parsed.match(strPattern1);
+		var res2 = parsed.match(strPattern1);
 		parsed = RegExS(res2, parsed);
 
-		let res3 = parsed.match(strPattern2);
+		var res3 = parsed.match(strPattern2);
 		parsed = RegExS(res3, parsed);
 
-        element.innerHTML = parsed;
+        codeElements[i].innerHTML = parsed;
+	}
+    // codeElements.forEach(element => {
         
-    });
+    
+        
+    // });
 }
 
 function RegExD(res, parsed) {
 
-	let p = parsed;
+	var p = parsed;
 	if (res != null) {
 		res.forEach(item => {
-			let n = item.replace(keywordsPattern, "<span id='comment'>$1</span>");
+			var n = item.replace(keywordsPattern, "<span id='comment'>$1</span>");
 			n = n.replace(funcPattern, "<span id='comment'>$1</span>");
 			n = n.replace(numbersPattern, "<span id='comment'>$1</span>");
 			p = p.replace(item, n);
@@ -55,10 +58,10 @@ function RegExD(res, parsed) {
 
 function RegExS(res, parsed) {
 
-	let p = parsed;
+	var p = parsed;
 	if (res != null) {
 		res.forEach(item => {
-			let n = item.replace(keywordsPattern, "<span id='string'>$1</span>");
+			var n = item.replace(keywordsPattern, "<span id='string'>$1</span>");
 			n = n.replace(funcPattern, "<span id='string'>$1</span>");
 			n = n.replace(numbersPattern, "<span id='string'>$1</span>");
 			p = p.replace(item, n);
@@ -66,4 +69,5 @@ function RegExS(res, parsed) {
 	}
 	return p;
 }
+
 window.onload = pySyntax();
