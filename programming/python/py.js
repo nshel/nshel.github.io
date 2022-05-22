2da90e68b40ee1d85dade37402372b88f474463a
nshel <105758041+nshel@users.noreply.github.com>
2022-05-22 02:12:42 -0700

    Add files via upload

--- programming/python/py.js
+++ programming/python/py.js
@@ -1,5 +1,5 @@
 // Patterns
-var keywordsPattern = /\b(import|from|if|else|elif|try|except|for|in|as|class|with|def|return|break|continue)(?![\w])/g,
+var keywordsPattern = /\b(import|from|if|else|elif|try|except|for|in|as|class|while|True|False|with|def|return|break|continue)(?![\w])/g,
 funcPattern = /\b(print|open|read|range|write|len|exit|input|str|int|self)(?![\w])/g,
 strPattern1 = /(".*?")/g,
 strPattern2 = /('.*?')/g,
