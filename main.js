047dc509c6de9a91478cd62ececc8e5ce4b3e36d
nshel <105758041+nshel@users.noreply.github.com>
2022-10-08 13:03:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+    /***
+     * Sticky header on scroll
+     */
+    const selectHeader = document.querySelector('#header');
+    if (selectHeader) {
+        document.addEventListener('scroll', () => {
+            window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+        });
+    }
+
+    /**
+   * Mobile nav toggle
+   */
+    
+    const mobileNavShow = document.querySelector('mobile-nav-show');
+    const mobileNavHide = document.querySelector('mobile-nav-hide');
+
+    document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+        el.addEventListener('click', function(event) {
+            event.preventDefault();
+            mobileNavToggle();
+        });
+    });
+
+    function mobileNavToggle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavShow.classList.toggle('d-none');
+        mobileNavHide.classList.toggle('d-none');
+    }
+
+      /**
+     * Animation on scroll function and init
+     */
+    function aos_init() {
+        AOS.init({
+        duration: 800,
+        easing: 'slide',
+        once: true,
+        mirror: false
+        });
+    }
+    window.addEventListener('load', () => {
+        aos_init();
+    });
+    
+});
