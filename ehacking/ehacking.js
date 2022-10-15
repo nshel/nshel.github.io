10124a6f1dd2a418459c718500fc228db4e59ed5
nshel <105758041+nshel@users.noreply.github.com>
2022-10-14 18:23:31 -0700

    Add files via upload

---
+++ ehacking/ehacking.js
@@ -0,0 +1,29 @@
+// Patterns
+var keywordsPattern = /\b(nmap|sshcred|hydra|msfconsole)(?![\w])/g,
+strPattern1 = /(".*?")/g,
+strPattern2 = /('.*?')/g,
+argPattern = /(\s)(?=[-.*?])/g;
+
+function commandSyntax() {
+
+    // get all elements with id 'py-code'
+    var codeElements = document.querySelectorAll("[id=command]");
+	for (var i = 0; i < codeElements.length; i++) {
+		var parsed = codeElements[i].innerHTML;
+        parsed = parsed.replace(strPattern1, '<span id="string">$1</span>');
+        parsed = parsed.replace(strPattern2, "<span id=\"string\">$1</span>");
+        parsed = parsed.replace(keywordsPattern, '<span id="keyword">$1</span>');
+        parsed = parsed.replace(argPattern, '<span id="arg">$1</span>');
+
+        codeElements[i].innerHTML = parsed;
+	}
+    // codeElements.forEach(element => {
+        
+    
+        
+    // });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    commandSyntax();
+});
